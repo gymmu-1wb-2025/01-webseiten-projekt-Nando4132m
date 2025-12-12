d597b2cd0da7808134460d4ec9abebaef7d732d2
Nando Stöckli <e228448@edu.sbl.ch>
2025-12-12 15:21:07 +0000

    WIP: anwendung des javascriptes üben, taschenrechner für weitere unterseite, lernen mit website

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ function createNewElement() {
   let titleElement = document.querySelector("header");
   titleElement.textContent = count;
 }
+
+var operand1 = parseInt(document.getElementById("operand1").value);
+
+function calculate() {
+  var operand1 = parseInt(document.getElementById("operand").value);
+  var operand2 = parseInt(document.getElementById("operand").value);
+  var result = 0;
+}
