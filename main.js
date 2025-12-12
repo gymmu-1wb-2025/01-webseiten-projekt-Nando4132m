665e30024905e168678262b622283e73954c1ac4
Nando Stöckli <e228448@edu.sbl.ch>
2025-12-12 15:43:32 +0000

    Erster Teil Javascript Taschenrechner glernt von website jetzt noch funktionabel machen

--- main.js
+++ main.js
@@ -14,4 +14,22 @@ function calculate() {
   var operand1 = parseInt(document.getElementById("operand").value);
   var operand2 = parseInt(document.getElementById("operand").value);
   var result = 0;
+  switch (document.getElementById("operator").value) {
+    case "plus":
+      result = operand1 + operand2;
+      break;
+    case "minus":
+      result = operand1 - operand2;
+      break;
+    case "devide":
+      result = operand1 / operand2;
+      break;
+    case "multiply":
+      result = operand1 * operand2;
+      break;
+    default:
+      result = operand1 + operand2;
+      break;
+  }
+  document.getElementById("result").innerHTML = "Das Ergebnis ist: " + result;
 }
